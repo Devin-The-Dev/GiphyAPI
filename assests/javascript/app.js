634fc1ce3f27fd607c9447c5a4905e6e4163c197
Devin Udy <devin.udy@gmail.com>
2018-12-05 16:53:22 -0700

    reformated reset.css, generated a button with word or phrase from the user's value

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,20 +1,31 @@
 /*Objectives:
- - add buttons of saved gifs
+ - have buttons generate gifs from user input
+ - change gifs to a fixed width and height
 */
-function getData() {
-  var input = $("#userInput")
-    .val()
-    .trim();
+//Upon enter, the code goes to a new page displaying my code. There needs to be "event" somewhere
+//Can only get one set of gifs from user. If user types a different word, I want new gifs to appear
+//Want to display gifs if user clicks submit button or presses enter. Used this source https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp but no success
+$('#userInputButton').on('click', function getData(event) {
+  event.preventDefault();
+  var input = $("#userInput").val().trim();
 
   var xhr = $.get(
-    "http://api.giphy.com/v1/gifs/search?q=cat&api_key=IjkESGH2V6bmed1LWaCukFWZFrOthKwd&limit=30"
+    "http://api.giphy.com/v1/gifs/search?q="
+    + input +
+    "&api_key=IjkESGH2V6bmed1LWaCukFWZFrOthKwd&limit=30"
   );
-  xhr.done(function(response) {
+
+  xhr.done(function (response) {
     console.log("success got data", response);
     var gifs = response.data;
-    for (i in gifs) {
-      //for in loops are when you want to loop objects
+    for (i in gifs) { // for/in loops are when you want to loop an object
       $("#giphy").append("<img src = '" + gifs[i].images.original.url + "'/>");
-    }
+    };
   });
-}
+
+  $('#buttons').append("<button type='button' id='singleButton'>"
+    + input +
+    "</button>"
+  ).on('click', function getData() { }); //Run an if statement instead?
+
+});
